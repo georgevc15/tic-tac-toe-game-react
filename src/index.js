692714109f35020e0a65cbbfb3f5bc4c5e10e92f
George Codat Tudoran <office@solcreation.ro>
2018-11-14 14:34:15 +0200

    Declaring a winner

--- src/index.js
+++ src/index.js
@@ -26,6 +26,7 @@ function Square(props) {
             squares: patrate,
             xIsNext: !this.state.xIsNext,
         });
+        //console.log(patrate);
     }
 
     renderSquare(i) {
@@ -38,8 +39,14 @@ function Square(props) {
       }
   
     render() {
-      const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O') ;
-  
+      const winner = calculateWinner(this.state.squares);
+      let status;
+      if (winner) {
+        status = 'Winner:' + winner;
+      }  else {
+        status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O') ;
+      }
+        
       return (
         <div>
           <div className="status">{status}</div>
@@ -79,6 +86,26 @@ function Square(props) {
     }
   }
   
+  function calculateWinner(squares) {
+      const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+      ];
+      for (let i = 0; i < lines.length; i++) {
+          const [a, b, c] = lines[i];
+          if(squares[a]  === squares[b] && squares[a]  === squares[c] && squares[b]  === squares[c] && squares[b]  === squares[a]) {
+              return squares[a];
+          }
+      }
+      return null;
+  }
+
   // ========================================
   
   ReactDOM.render(
